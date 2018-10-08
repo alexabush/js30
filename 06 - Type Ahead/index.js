10aaa460b2d8d9acd3836450825afad553a1201b
Alex Bush <alexabush7@gmail.com>
2018-10-08 14:12:15 -0700

    completed core filter and display functionality

---
+++ 06 - Type Ahead/index.js
@@ -0,0 +1,37 @@
+console.log('connected1');
+$(async function() {
+  console.log('connected');
+  const endpoint =
+    'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+  let $suggestions = $('.suggestions');
+  let res = await fetch(endpoint);
+  let cities = await res.json();
+  let $search = $('.search');
+
+  $search.on('keyup', e => {
+    let val = $(e.target).val();
+    let filteredCities = filterCities(val);
+    let cityLis = filteredCities.map(city => {
+      let cityLi = $('<li>')
+        .addClass('suggestions')
+        .append(
+          $('<p>').text(`${city.city}, ${city.state}`),
+          $('<p>').text(addCommas(city.population))
+        );
+      return cityLi;
+    });
+    $suggestions.empty();
+    $suggestions.append(...cityLis);
+  });
+
+  function addCommas(num) {
+    //add proper commas to num
+    return num;
+  }
+
+  function filterCities(substring) {
+    return cities.filter(city => {
+      return city.city.toLowerCase().includes(substring.toLowerCase());
+    });
+  }
+});
