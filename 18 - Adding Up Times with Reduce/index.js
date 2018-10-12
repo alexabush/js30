fae061fc45189bb0ec2e7af70143c83f5dc5a78e
Alex Bush <alexabush7@gmail.com>
2018-10-11 17:41:56 -0700

    cleaned up logic a little bit

--- 18 - Adding Up Times with Reduce/index.js
+++ 18 - Adding Up Times with Reduce/index.js
@@ -1,25 +1,36 @@
 $(function() {
-  let $videos = $('.videos');
-  let times = Array.from($('.videos li')).map(li => {
-    return $(li).data('time');
-  });
-  let total = times.reduce((acc, currentTime) => {
-    let [minutes, seconds] = currentTime.split(':');
-    let total = +seconds + +minutes * 60;
-    return acc + total;
-  }, 0);
-  let minutes = Math.floor(total / 60);
-  let seconds = total % 60;
-  let hours;
-  if (minutes > 60) {
-    hours = Math.floor(minutes / 60);
-    minutes = minutes % 60;
-  }
+  let totalSeconds = Array.from($('.videos li'))
+    .map(li => {
+      return $(li).data('time');
+    })
+    .reduce((acc, currentTime) => {
+      let [minutes, seconds] = currentTime.split(':');
+      let total = +seconds + +minutes * 60;
+      return acc + total;
+    }, 0);
 
-  if (hours) {
-    console.log(`${padTime(hours)}:${padTime(minutes)}:${padTime(seconds)}`);
-  } else {
-    console.log(`${padTime(minutes)}:${padTime(seconds)}`);
+  console.log(formatTime(totalSeconds));
+
+  function formatTime(seconds) {
+    let hours;
+    let minutes;
+    if (seconds > 60) {
+      isMinutes = true;
+      minutes = Math.floor(seconds / 60);
+      seconds = seconds % 60;
+    }
+    if (minutes > 60) {
+      isHours = true;
+      hours = Math.floor(minutes / 60);
+      minutes = minutes % 60;
+    }
+    if (isHours && isMinutes) {
+      return `${padTime(hours)}:${padTime(minutes)}:${padTime(seconds)}`;
+    } else if (isMinutes) {
+      return `${padTime(minutes)}:${padTime(seconds)}`;
+    } else {
+      return `${padTime(seconds)}`;
+    }
   }
 
   function padTime(time) {
